c1cddc58abb4054bec52b5433ea08d3e2abc2a6d
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 17:56:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  if block_given?
+    i = 0 
+    array2 = []
+    while i < array.length
+      array2.push(yield(array[i]))
+      i += 1
+    end
+    array2
+  else
+    puts "Ooops no block given, but thats okay :)."
+  end
 
-
+end
